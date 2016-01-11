c45c6043531697b8219becf61ad84f63687fcfe0
Suui <suui.gd@gmail.com>
2016-01-11 02:12:11 +0100

    Raising Events

--- Pluralsight.EventsDelegatesAndLambdas.Creation/Worker.cs
+++ Pluralsight.EventsDelegatesAndLambdas.Creation/Worker.cs
@@ -12,7 +12,22 @@ namespace Pluralsight.EventsDelegatesAndLambdas.Creation
 
 		public void DoWork(int workHours, WorkType workType)
 		{
-			
+			for (var hours = 1; hours <= workHours; hours++)
+			{
+				OnWorkPerformed(hours, workType);
+			}
+			OnWorkCompleted();
+		}
+
+		protected virtual void OnWorkPerformed(int workHours, WorkType workType)
+		{
+			// We are actually invoking the delegate
+			WorkPerformed?.Invoke(workHours, workType);
+		}
+
+		protected virtual void OnWorkCompleted()
+		{
+			WorkCompleted?.Invoke(this, EventArgs.Empty);
 		}
 	}
 }
